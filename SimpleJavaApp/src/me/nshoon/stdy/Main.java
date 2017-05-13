10ba1c10d674527f23476e723c3f5d48e08f874b
nshoon <nsh0110@gmail.com>
2017-05-13 23:28:41 +0900

    sample

---
+++ SimpleJavaApp/src/me/nshoon/stdy/Main.java
@@ -0,0 +1,17 @@
+package me.nshoon.stdy;
+
+/**
+ * The type Main.
+ */
+public class Main {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
